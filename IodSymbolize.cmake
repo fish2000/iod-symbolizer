8917c9daa606b1a037a1a1cd3e35ff247356ce33
Alexander Böhn <fish2000@gmail.com>
2016-01-20 19:32:06 -0500

    IOD_SYMBOLIZE() cmake macro now supports multiple arguments

--- IodSymbolize.cmake
+++ IodSymbolize.cmake
@@ -1,17 +1,21 @@
 
-# macro(IOD_SYMBOLIZE_DIR directory)
-# endmacro()
-#
-# macro(ADD_IOD_SYMBOLS)
-# endmacro()
+include(FindPythonInterp)
 
-macro(IOD_SYMBOLIZE dir0 dir1 header)
-add_custom_command(
-    OUTPUT ${header}
-    COMMAND symbolizer --verbose ${dir0} ${dir1} -o ${header})
-add_custom_target("iod_symbolize"
-    DEPENDS ${header})
-set_source_files_properties(
-    ${header}
-    PROPERTIES GENERATED TRUE)
+macro(IOD_SYMBOLIZE header)
+    set(symbolizer_args ${ARGN})
+    list(LENGTH symbolizer_args num_dir_args)
+    if(NOT ${num_dir_args} GREATER 0)
+        message(FATAL_ERROR
+            "IOD_SYMBOLIZE() called without directories")
+    endif()
+    list(INSERT symbolizer_args 0 symbolizer --verbose)
+    list(APPEND symbolizer_args -o ${header})
+    add_custom_command(
+        OUTPUT ${header}
+        COMMAND ${symbolizer_args})
+    add_custom_target("iod_symbolize"
+        DEPENDS ${header})
+    set_source_files_properties(
+        ${header}
+        PROPERTIES GENERATED TRUE)
 endmacro()
