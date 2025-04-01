ccb3691e4f2ed3d3cb2514215a99312c38e20143
Alexander Böhn <fish2000@users.noreply.github.com>
2025-04-01 14:55:06 -0400

    Updates to use the installed “symbolizer” Python package

--- symbolizer/__init__.py
+++ symbolizer/__init__.py
@@ -1,14 +1,14 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 
-from parser import (
+from symbolizer.parser import (
     SUFFIXES, KEYWORDS, SYMBOL_PREFIX,
     sanitizer, symbol_re, suffix_re,
     quotes, dbl_quotes, cpp_comments, c_comments, blockout,
     collect_files, sanitize_suffixes, collect,
     parse, parse_source_file)
 
-from parser import Parser
-from templates import (
+from symbolizer.parser import Parser
+from symbolizer.templates import (
     generate_header,
     u as symbolizer_unicode)
